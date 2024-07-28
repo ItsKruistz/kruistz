53d3a03b2f06185e56b992c7acba7a53dd40761c
ItsKruistz <lekruistz@gmail.com>
2024-07-29 06:37:58 +0700

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,30 @@
 <main>
+
+<nav class="flex items-center justify-between flex-wrap bg-teal-500 sticky z-10 top-0 p-2">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <button class="flex items-center justify-center h-11 w-11 rounded-full text-white hover:bg-teal-400">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current" viewBox="0 -960 960 960">
+        <path d="M160-240q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Zm0-200q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Zm0-200q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Z"/>
+      </svg>
+    </button>
+    <span class="font-medium text-xl ml-2.5">Title</span>
+  </div>
+  <div class="block lg:hidden">
+    <button class="flex items-center justify-center h-11 w-11 rounded-full text-white hover:bg-teal-400">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current" viewBox="0 -960 960 960">
+        <path d="M160-240q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Zm0-200q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Zm0-200q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Z"/>
+      </svg>
+    </button>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+  </div>
+</nav>
+
   <section>
-    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col">
+    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
       <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-left text-left">
         <h1 class="text-4xl mb-4 font-bold">Hello, I'm Kruistz!</h1>
-        <p class="mb-8 leading-normal text-lg">An hobbyist coder who likes to play gacha games. I also like to take some photos too!</p>
+        <p class="mb-8 leading-normal text-lg">An hobbyist coder who likes to play gacha games. I also like to take photos too!</p>
       <div class="flex justify-center">
       </div>
       </div>
@@ -12,4 +33,5 @@
     </div>
     </div>
   </section>
+
 </main>
