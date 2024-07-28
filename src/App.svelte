2b59a023ecb236828824ef08cf8b12e32c8e0930
ItsKruistz <lekruistz@gmail.com>
2024-07-29 06:50:20 +0700

    iphonsz

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 <nav class="flex items-center justify-between flex-wrap bg-teal-500 sticky z-10 top-0 p-2">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
     <button class="flex items-center justify-center h-11 w-11 rounded-full text-white hover:bg-teal-400">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="h-5 w-5 fill-current" viewBox="0 0 24 24">
         <path d="M3 12h18M3 6h18M3 18h18"/>
       </svg>
     </button>
