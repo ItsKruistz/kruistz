fc8d8452806c27d5e772004cabdc79b7fb94d826
ItsKruistz <lekruistz@gmail.com>
2024-07-29 06:20:33 +0700

    h

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,15 @@
 <main>
-  <h1 class="text-4xl font-bold text-cyan-400">Test</h1>
+  <section>
+    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col">
+      <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-left text-left">
+        <h1 class="text-4xl mb-4 font-bold">Hello, I'm Kruistz!</h1>
+        <p class="mb-8 leading-normal text-lg">An hobbyist coder who likes to play gacha games. I also like to take some photos too!</p>
+      <div class="flex justify-center">
+      </div>
+      </div>
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
+    </div>
+    </div>
+  </section>
 </main>
