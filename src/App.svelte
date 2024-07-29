60575d650a1b02d811a2bbbdb2b8f4507f6958ee
ItsKruistz <lekruistz@gmail.com>
2024-07-29 14:39:08 +0700

    dgd

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
 
 <nav class="flex items-center justify-between flex-wrap bg-teal-500 sticky z-10 top-0 p-2">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <button class="flex items-center justify-center h-11 w-11 rounded-full text-white hover:bg-teal-400">
+    <button class="flex items-center justify-center h-11 w-11 rounded-full text-white hover:bg-teal-400 duration-150">
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="h-[22px] w-[22px]" viewBox="0 0 24 24">
         <path d="M3 12h18M3 6h18M3 18h18"/>
       </svg>
