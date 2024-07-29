57cdd84ea6c247d123b8db8bbe5881f312128475
ItsKruistz <lekruistz@gmail.com>
2024-07-29 16:12:59 +0700

    Dark mode

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
-<main>
+<main class="dark:bg-black">
 
 <nav class="flex items-center justify-between flex-wrap bg-white dark:bg-black sticky z-10 top-0 p-2">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <button class="flex items-center justify-center h-11 w-11 rounded-full text-white dark:text-black hover:bg-teal-400 duration-150" aria-label="Open menu">
+    <button class="flex items-center justify-center h-11 w-11 rounded-full text-white hover:bg-neutral-200 duration-150" aria-label="Open menu">
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="h-[22px] w-[22px]" viewBox="0 0 24 24">
         <path d="M3 12h18M3 6h18M3 18h18"/>
       </svg>
@@ -19,7 +19,7 @@
 
   <section>
     <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-      <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-left text-left dark:text-black">
+      <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-left text-left dark:text-white">
         <h1 class="text-4xl mb-4 font-bold">Hello, I'm Kruistz!</h1>
         <p class="mb-8 leading-normal text-lg">An hobbyist coder who likes to play gacha games. I also like to take photos too!</p>
       <div class="flex justify-center">
