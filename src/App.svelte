ab3a885f0fccb76de1d719a8d63c812d572d6cc6
ItsKruistz <lekruistz@gmail.com>
2024-07-30 10:27:53 +0700

    Cosmetics

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,14 @@
 
 <nav class="flex items-center justify-between flex-wrap bg-white dark:bg-[#101010] sticky z-10 top-0 p-2">
   <div class="flex items-center flex-shrink-0 text-black dark:text-white mr-6">
-    <label for="my-drawer" class="drawer-button flex items-center justify-center h-11 w-11 rounded-full hover:opacity-75 duration-150" aria-label="Open menu">
+    <label for="my-drawer" class="drawer-button flex items-center justify-center h-11 w-11 rounded-full hover:opacity-50 duration-150" aria-label="Open menu">
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="h-[22px] w-[22px]" viewBox="0 0 24 24">
         <path d="M3 12h18M3 6h18M3 18h18"/>
       </svg>
     </label>
     <span class="font-semibold text-xl ml-2.5">Title</span>
     </div>
-    <button class="flex items-center justify-center h-11 w-11 text-black dark:text-white rounded-full hover:opacity-75 duration-150" aria-label="Search">
+    <button class="flex items-center justify-center h-11 w-11 text-black dark:text-white rounded-full hover:opacity-50 duration-150" aria-label="Search">
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="h-[22px] w-[22px]" viewBox="0 0 24 24">
         <circle cx="11" cy="11" r="8"/>
         <path d="m21 21-4-4"/>
@@ -22,10 +22,10 @@
   </div>
   <div class="drawer-side z-20">
     <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-    <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+    <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-3">
       <!-- Sidebar content here -->
-      <li><a class="text-red-300">Home</a></li>
-      <li><a>Sidebar Item 2</a></li>
+      <li><a class="text-red-300 text-xl p-2 rounded-lg hover:bg-white text-black duration-150">Home</a></li>
+      <li><a class="text-red-300 text-xl p-2 rounded-lg hover:bg-white text-black duration-150">Sidebar Item 2</a></li>
     </ul>
   </div>
 
